d6dbf94233f8fb49df17daa99f21f27aea01fd92
Matteo Cirami <mcirami@users.noreply.github.com>
2024-09-13 17:24:19 -0500

    started changing shopify integration form

--- resources/js/Pages/Dashboard/Components/Link/Forms/Shopify/ShopifyIntegration.jsx
+++ resources/js/Pages/Dashboard/Components/Link/Forms/Shopify/ShopifyIntegration.jsx
@@ -1,48 +1,57 @@
-import React, {useState} from 'react';
-import {setStorage} from '@/Services/LinksRequest.jsx';
+import React, { useState } from "react";
+import { setStorage } from "@/Services/LinksRequest.jsx";
 
 const ShopifyIntegration = ({
-                                connectionError,
-                                integrationType,
-                                editID,
-                                showAddStore,
-                                setShowAddStore,
-                                pageID
+    connectionError,
+    integrationType,
+    editID,
+    showAddStore,
+    setShowAddStore,
+    pageID,
 }) => {
-
     const [domain, setDomain] = useState(null);
 
-    const handleSubmit = (e) => {
+    /* const handleSubmit = (e) => {
         e.preventDefault();
 
         if (domain) {
-
             const url = "/auth/shopify?domain=" + domain;
             let myPromise = new Promise((resolve, reject) => {
                 setStorage(editID, integrationType, pageID);
                 resolve(url);
                 reject("Error");
-            })
+            });
 
             myPromise.then(
                 function (value) {
-                    window.location.href = value
+                    window.location.href = value;
                 },
-                function(error) {
+                function (error) {
                     console.error(error);
-                }
-            )
+                },
+            );
         }
-    }
+    }; */
 
     return (
         <div className="integration_wrap">
             <h3>Add your Shopify products as a LinkPro button!</h3>
-            <p className="mb-4">In connecting Shopify, you are sharing your Shopify store name and Product details with LinkPro.</p>
-            <p className="small my-4">Note: You will be redirected away from LinkPro to log into Shopify. You will need to either already have or create a a Shopify store of your own to use this integration.</p>
+            <p className="mb-4">
+                In connecting Shopify, you are sharing your Shopify store name
+                and Product details with LinkPro.
+            </p>
+            <p className="small my-4">
+                Note: You will be redirected away from LinkPro to log into
+                Shopify. You will need to either already have or create a a
+                Shopify store of your own to use this integration.
+            </p>
 
-            <form id="scrollTo" onSubmit={handleSubmit} className="link_form shopify_domain">
-                <label htmlFor="domain">Enter your .myshopify.com URL to log into your store.</label>
+            <div
+                id="scrollTo"
+                /* onSubmit={handleSubmit} */
+                className="link_form shopify_domain"
+            >
+                {/* <label htmlFor="domain">Enter your .myshopify.com URL to log into your store.</label>
                 <div className="input_wrap">
                     <input type="text"
                            name="domain"
@@ -51,17 +60,48 @@ const ShopifyIntegration = ({
                            required
                     />
                     <p>.myshopify.com</p>
+                </div> */}
+                <div className="text_wrap">
+                    <p>
+                        In order to connect your Shopify store to LinkPro, you
+                        will need to install the LinkPro app from the Shopify
+                        App Marketplace.
+                    </p>
+                    <ol>
+                        <li>
+                            <p>
+                                Click button below to install the app from the
+                                Shopify marketplace
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                Open the LinkPro app in your Shopy store admin
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                Click "Connect Your LinkPro Account" button and
+                                follow the prompts
+                            </p>
+                        </li>
+                    </ol>
                 </div>
-
                 <div className="button_wrap">
-                    <button className="button blue" type="submit">
-                        Login To Shopify
-                    </button>
+                    <a
+                        href="https://apps.shopify.com/linkpro"
+                        className="button blue"
+                    >
+                        Go To Shopify App Marketplace
+                    </a>
                 </div>
-                {showAddStore &&
+                {showAddStore && (
                     <div className="button_wrap mt-3">
-                        <a href="#" className="button transparent gray" type="submit"
-                            onClick={(e) =>{
+                        <a
+                            href="#"
+                            className="button transparent gray"
+                            type="submit"
+                            onClick={(e) => {
                                 e.preventDefault();
                                 setShowAddStore(false);
                             }}
@@ -69,12 +109,15 @@ const ShopifyIntegration = ({
                             Cancel
                         </a>
                     </div>
-                }
-                <small>LinkPro will not receive any sales data. All transactions will occur on your Shopify Store.</small>
-            </form>
-            <div className="connection_error">
-                <p>{connectionError}</p>
+                )}
+                <small>
+                    LinkPro will not receive any sales data. All transactions
+                    will occur on your Shopify Store.
+                </small>
             </div>
+            {/* <div className="connection_error">
+                <p>{connectionError}</p>
+            </div> */}
         </div>
     );
 };
